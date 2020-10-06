bf1a769db1fcd3eba1c9bd2d849eda7c5d714095
Devin Wilson <craigvin84@yahoo.com>
2020-10-06 21:44:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,12 +16,13 @@ end
 
 
 def assign_rooms(speakers)
-  rooms=[]
-  speakers.each_with_index{|name,index|
-  rooms.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
-}
-return rooms
-end
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
 
 #names.collect {|badge| "Hello, my name is "badge"."}
 #end

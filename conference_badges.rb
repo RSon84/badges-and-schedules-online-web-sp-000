bb40188818af19eaefdba26a328cc9922d8c9d53
Devin Wilson <craigvin84@yahoo.com>
2020-10-06 21:17:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
 names = %w(Edsger Ada Charles Alan Grace Linus)
 def batch_badge_creator(names)
-names.collect {|badge| print "Hello, my name is badge."}
+names.collect {|badge| print "Hello, my name is "badge"."}
 end
